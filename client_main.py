b4b19ffa33b6c30dddad4db835aeea5fb330dec5
malikbredehoeft <131286836+malikbredehoeft@users.noreply.github.com>
2023-10-11 13:44:15 +0200

    Update client_main.py

--- client_main.py
+++ client_main.py
@@ -1,10 +1,9 @@
-from shared.RabbitMQConnector import *
 
 # Einlesen der Daten vom Nutzer
 user_input = input("Land: ") + ";"
+
 user_input += input("Stadt: ") + ";"
 user_input += input("Straße: ") + ";"
 user_input += input("Hausnummer: ")
 
-
-
+print(user_input)
