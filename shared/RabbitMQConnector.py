b688376cd3e15f6aebc0b3e0c5bb80e2edebfaf1
Leon Gauweiler <leon.gauweiler@gmail.com>
2023-10-11 09:43:12 +0200

    added: send message in queue

--- shared/RabbitMQConnector.py
+++ shared/RabbitMQConnector.py
@@ -13,3 +13,8 @@ class RabbitMQConnector:
 
     def queue_exists(self, queue_name):
         return self.queues.__contains__(queue_name)
+
+    def send_message(self, queue_name, message):
+        if not self.queue_exists(queue_name):
+            self.create_queue(queue_name)
+        self.channel.basic_publish(exchange="", routing_key=queue_name, body=message)
