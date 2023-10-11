78fdfb19498fa9859a5fc47c3b05becaf8252da8
Leon Gauweiler <leon.gauweiler@gmail.com>
2023-10-11 11:50:44 +0200

    added: callAPI

--- server/WeatherAPIConnector.py
+++ server/WeatherAPIConnector.py
@@ -1,3 +1,15 @@
+import datetime
+import time
+
+
 class WeatherAPIConnector:
     def __init__(self):
         pass
+    def call_api(self):
+        apiCall = False
+        if apiCall == False:
+            next_call = (datetime.datetime.fromisoformat(zeit_aus_api) - datetime.datetime.now()).total_seconds() + 10
+            time.sleep(next_call)
+            return self.call_api()
+        else:
+            return apiCall
