7e7ce092681ffd8212002afbc8fe8b8dea91e566
malikbredehoeft <131286836+malikbredehoeft@users.noreply.github.com>
2023-10-11 12:07:56 +0200

    Update main.py

--- client/main.py
+++ client/main.py
@@ -1,8 +1,13 @@
 from shared.RabbitMQConnector import *
 
+# Hier muss vom Nutzer die Adresse abgefragt werden
+userinput =
+
+
 rabbit = RabbitMQConnector("localhost")
+
 while True:
-    # Hier muss vom Nutzer die Adresse abgefragt werden
+
     daten = "Deutschland:Karlsruhe:Bahnhofsstrasse 4a:0:0"
     message = rabbit.send_message("waitinglist", daten)
     response = rabbit.wait_for_message("waitinglist")
