35ee28d4781b070f8228d050eca1b46078e5aa89
Leon Gauweiler <leon.gauweiler@gmail.com>
2023-10-11 08:58:01 +0200

    added: queue commands

--- shared/RabbitMQConnector.py
+++ shared/RabbitMQConnector.py
@@ -1,3 +1,15 @@
+import pika
+
+
 class RabbitMQConnector:
     def __init__(self):
-        pass
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.channel = self.connection.channel()
+        self.queues = []
+
+    def create_queue(self, queue_name):
+        self.channel.queue_declare(queue=queue_name)
+        self.queues.append(queue_name)
+
+    def queue_exists(self, queue_name):
+        return self.queues.__contains__(queue_name)
